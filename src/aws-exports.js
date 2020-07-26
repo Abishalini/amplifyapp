65c38e64f6e6cb16671486fc2c577e214881f963
Abishalini <abi.gpuram@gmail.com>
2020-07-25 22:47:32 -0500

    Notes

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_4tZP0FK1D",
     "aws_user_pools_web_client_id": "q6tq45b5f91o17c8ti8onpob4",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://fxsjp52p5ffo5bbfd5xwfcgqoe.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-kq2ldh4r5ncnpadpx53lwure3i"
 };
 
 
